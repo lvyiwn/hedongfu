e9bd43cddf9e4e57829bc088e306e13358afc1a2
Your Name <lvyiwangning@yeah.net>
2016-05-13 18:52:54 +0800

    test

--- app.cmd
+++ app.cmd
@@ -1,24 +1,26 @@
-1. 切换到淘宝源
+
+%切换到淘宝源%
 $ npm install -g cnpm --registry=https://registry.npm.taobao.org
 
-2. 切换到项目目录
+%切换到项目目录%
 E:
 cd E:\Projects\My\HedongFu
 
-3. 创建目录命令
 $ cnpm install -g vue-cli
 $ vue init webpack my-project
 $ cd my-project
 $ cnpm install
 $ cnpm run dev
 
-
------------------------------创建项目后使用命令-------------------------
-
-
-E:
-cd E:\Projects\My\HedongFu\app
-
 ssh GitAdmin@192.168.1.92
 
-cnpm run dev
+
+%               创建项目后使用命令               %
+
+E:
+cd E:\Projects\My\HedongFu\app ::进入项目目录
+
+
+cnpm run dev ::启动
+
+git push -u origin master ::推送
